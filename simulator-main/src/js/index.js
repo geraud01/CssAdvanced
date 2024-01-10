6de26de7f41ed922078ddbd1c38758c7f8adeeae
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-01-10 17:15:56 -0300

    Aula III

--- simulator-main/src/js/index.js
+++ simulator-main/src/js/index.js
@@ -1,4 +1,20 @@
 import '../scss/styles.scss'
+import imageBuilding from '../assets/owen-lystrup-boLgiM0qwkg-unsplash.jpg'
+import imageNotebook from '../assets/mike-tinnion-3ym6i13Y9LU-unsplash.jpg'
 
 const elemRoot = document.getElementById('root')
 elemRoot.classList.add('container')
+
+
+const elemImageBuilding = new Image()
+elemImageBuilding.src = imageBuilding
+
+const elemImageNotebook = new Image()
+elemImageNotebook.src = imageNotebook
+
+
+document.querySelector('#building')
+    .appendChild(elemImageBuilding)
+
+document.querySelector('#Notebook')
+    .appendChild(elemImageNotebook)
